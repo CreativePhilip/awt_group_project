6ed9cc3efd4494a6cbfacd0f51adbe87880a268b
bszewc3203 <bszewczyk3203@gmail.com>
2023-04-02 22:13:15 +0200

    changed routing to proper react elements

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,15 +1,17 @@
 import { createBrowserRouter } from "react-router-dom"
 import { CalendarViewRoot }    from "./views/CalendarViewRoot"
+import LoginInterface from "./components/LoginInterface"
+import RegisterInterface from "./components/LoginInterface"
 
 
 export const router = createBrowserRouter([
     {
         path   : "/login",
-        element: <div>Hello world! Login</div>,
+        element: <div><LoginInterface/></div>,
     },
     {
         path   : "/register",
-        element: <div> Hello world! Register </div>
+        element: <div><RegisterInterface/></div>
     },
     {
         path   : "/",
