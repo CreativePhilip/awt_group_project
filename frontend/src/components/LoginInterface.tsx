541be73b1844923ef7560bd75998b4633df644d7
bszewc3203 <bszewczyk3203@gmail.com>
2023-04-03 19:53:28 +0200

    Optimized imports and stylized error messages

--- frontend/src/components/LoginInterface.tsx
+++ frontend/src/components/LoginInterface.tsx
@@ -1,5 +1,4 @@
 import { Formik } from 'formik';
-import { useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from "yup";
 
@@ -25,14 +24,14 @@ function LoginInterface() {
                     <div>
                         <label className="text-xs font-light font-sans">E-mail</label><br/>
                         <input type="email" name="email" placeholder="name@email.com" value={formik.values.email} onChange={formik.handleChange} className="font-light text-base rounded border border-neutral-400 font-sans p-1"/>
-                        <div>{formik.errors.email && formik.touched.email ? formik.errors.email : null}</div>
+                        <div className="text-xs font-light font-sans text-red-500">{formik.errors.email && formik.touched.email ? formik.errors.email: null}</div>
                     </div>
                     <div>
                         <label className="text-xs font-light font-sans">Password</label><br/>
                         <input type="password" name="password" value={formik.values.password} onChange={formik.handleChange} className="font-light text-base rounded border border-neutral-400 font-sans p-1"/>
-                        <div>{formik.errors.password && formik.touched.password ? formik.errors.password : null}</div>
+                        <div className="text-xs font-light font-sans text-red-500">{formik.errors.password && formik.touched.password ? formik.errors.password : null}</div>
                     </div>
-                    <button type="submit" className="text-white font-sans font-light bg-blue-700 rounded focus:shadow-inner padding-300 py-1 px-2 mt-5">Log in</button>
+                    <button className="text-white font-sans font-light bg-blue-700 rounded focus:shadow-inner padding-300 py-1 px-2 mt-5" disabled={formik.isSubmitting}>Log in</button>
                     <button className="bg-white font-sans margin-10 rounded focus:shadow-inner py-1 px-2">Create an account</button>
                 </div>
             </div>
