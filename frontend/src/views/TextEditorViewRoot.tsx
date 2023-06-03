8e9b30bcc676c9b74c55a449aec84cd4220bcb46
olga <olga.szat@gmail.com>
2023-06-03 21:08:26 +0200

    integrated notes with backend

--- frontend/src/views/TextEditorViewRoot.tsx
+++ frontend/src/views/TextEditorViewRoot.tsx
@@ -2,15 +2,24 @@ import NavBar from "../components/NavBar";
 import TextEditor from "../components/TextEditor";
 import SecondaryButton from "../components/SecondaryButton";
 import PrimaryButton from "../components/PrimaryButton";
+import { addMeetingNote } from "../api/meetings";
+import { useState } from "react";
+
 
 export default function TextEditorViewRoot() {
+  const meetingId = 2;
+  const [note, setNote] = useState<string>("Insert your note here..");
+
+  const onConfirm = async () => {
+    await addMeetingNote(meetingId, {"note": note});
+  }
   const onCancel = () => {};
-  const onConfirm = () => {};
+
   return (
     <NavBar>
       <div className="flex justify-center">
         <div className="h-2/3 w-2/3 mt-4">
-          <TextEditor />
+          <TextEditor value={note} setValue={setNote}/>
         </div>
       </div>
       <div className="grid justify-items-end">
